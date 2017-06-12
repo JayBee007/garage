4649bca234154c710cc0262407d3eca793f75600
Jay <jaybee.codes@gmail.com>
2017-06-13 00:02:42 +0530

    correct path to resolve images & add HtmlWebpackPlugin

--- chatApp_dd/webpack.config.js
+++ chatApp_dd/webpack.config.js
@@ -2,6 +2,7 @@ import webpack from 'webpack'
 import path from 'path'
 
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 export default {
     devtool: 'eval-source-map',
@@ -32,7 +33,7 @@ export default {
             },
             {
                 test: /\.(jpe?g|png|woff|woff2|eot|ttf|svg|otf)$/,
-                include : path.join(__dirname, 'img'),
+                include : path.join(__dirname, 'src', 'app', 'img'),
                 use: [
                     {loader: 'url-loader',
                     options: {limit:40000}
@@ -43,6 +44,9 @@ export default {
         ]
     },
     plugins:[
-        new ExtractTextPlugin('styles.css')
+        new ExtractTextPlugin('styles.css'),
+        new HtmlWebpackPlugin ({
+            template : 'src/app/index.html'
+        })
     ]
 }
