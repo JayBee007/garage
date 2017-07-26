4b9f2edb18965d9e4e1035dd6567632e26ef72e1
Jay <jaybee.codes@gmail.com>
2017-07-26 23:44:41 +0300

    add onChange and onKeyPress handlers to Search Bar

--- musicmaster/src/components/App.js
+++ musicmaster/src/components/App.js
@@ -4,6 +4,22 @@ import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            query:''
+        }
+    }
+
+    search() {
+        console.log(this.state);
+    }
+
+    handleSearchChange(e) {
+        this.setState({
+            query: e.target.value
+        })
+    }
     render() {
         return(
             <div className="App">
@@ -14,8 +30,15 @@ class App extends Component {
                     <InputGroup>
                         <FormControl
                         type="text"
-                        placeholder="Search for an Artist" />     
-                        <InputGroup.Addon>
+                        placeholder="Search for an Artist"
+                        value={this.state.query}
+                        onChange={(e) => this.handleSearchChange(e)}
+                        onKeyPress={(e) => {
+                            if(e.key === 'Enter') {
+                                this.search();
+                            }
+                        }} />     
+                        <InputGroup.Addon onClick={() => this.search()}>
                             <Glyphicon glyph="search"></Glyphicon>
                         </InputGroup.Addon>
                     </InputGroup>
