53dbeec0e5b95bddf27d0ac7db8235354fe95e36
Jay <jaybee.codes@gmail.com>
2017-08-20 19:26:27 +0300

    add small text for conversion rate with date

--- currencyConverter/app/screens/Home.js
+++ currencyConverter/app/screens/Home.js
@@ -5,11 +5,14 @@ import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Buttons';
+import { LastConverted } from '../components/Text';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_CONVERSION_DATE = new Date();
+const TEMP_CONVERSION_RATE = 0.7974;
 
 class Home extends React.Component {
   handlePressBaseCurrency = () => console.log('base');
@@ -40,6 +43,12 @@ class Home extends React.Component {
           defaultValue={TEMP_QUOTE_PRICE}
           keyboardType="numeric"
           onChangeText={this.handleTextChange}
+        />
+        <LastConverted
+          base={TEMP_BASE_CURRENCY}
+          quote={TEMP_QUOTE_CURRENCY}
+          date={TEMP_CONVERSION_DATE}
+          conversionRate={TEMP_CONVERSION_RATE}
         />
         <ClearButton text="Reverse Currencies" onPress={this.handleSwapCurrency} />
       </Container>
