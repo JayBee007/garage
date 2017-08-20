9ce651c5a984e7d0090f1dc2ec8a829245a19510
Jay <jaybee.codes@gmail.com>
2017-08-20 19:13:16 +0300

    add reverse currency button

--- currencyConverter/app/screens/Home.js
+++ currencyConverter/app/screens/Home.js
@@ -4,6 +4,7 @@ import { StatusBar } from 'react-native';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
+import { ClearButton } from '../components/Buttons';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
@@ -11,11 +12,13 @@ const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
 
 class Home extends React.Component {
-  handlePressBaseCurrency = () => 'base';
+  handlePressBaseCurrency = () => console.log('base');
 
-  handlePressQuoteCurrency = () => 'quote';
+  handlePressQuoteCurrency = () => console.log('quote');
 
-  handlePressQuoteCurrency = text => text;
+  handlePressQuoteCurrency = text => console.log(text);
+
+  handleSwapCurrency = () => console.log('swap');
 
   render() {
     return (
@@ -38,6 +41,7 @@ class Home extends React.Component {
           keyboardType="numeric"
           onChangeText={this.handleTextChange}
         />
+        <ClearButton text="Reverse Currencies" onPress={this.handleSwapCurrency} />
       </Container>
     );
   }
