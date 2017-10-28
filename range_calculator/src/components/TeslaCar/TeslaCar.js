7e538cff0b28126f6dca90d3a554a5eb05faefe1
Jay <jaybee.codes@gmail.com>
2017-10-28 10:15:38 +0300

    add car and notice components

---
+++ range_calculator/src/components/TeslaCar/TeslaCar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './TeslaCar.css';
+
+const TeslaCar = (props) => (
+  <div className="tesla-car">
+    <div className="tesla-wheels">
+      <div className={`tesla-wheel tesla-wheel--front tesla-wheel--${props.wheelsize}`}></div>
+      <div className={`tesla-wheel tesla-wheel--rear tesla-wheel--${props.wheelsize}`}></div>
+    </div>
+  </div>
+);
+
+TeslaCar.prototypes = {
+  wheelsize: PropTypes.number
+}
+
+export default TeslaCar;
