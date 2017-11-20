48f80368e10a30a93559b757febc0364eeb39be6
Jay <jaybee.codes@gmail.com>
2017-11-20 22:10:53 +0300

    complete MVP functionality

--- targetSumMobileApp/src/components/Game.js
+++ targetSumMobileApp/src/components/Game.js
@@ -10,7 +10,7 @@ class Game extends React.Component {
   };
 
   state = {
-    selectedNumbers: [],
+    selectedIds: [],
   }
 
   randomNumbers = Array.from({length: this.props.randomNumberCount}).map(() => 1 + Math.floor(10 * Math.random()));
@@ -22,30 +22,49 @@ class Game extends React.Component {
     },0);
 
     isNumberSelected = (numberIndex) => {
-      return this.state.selectedNumbers.indexOf(numberIndex) >= 0;
+      return this.state.selectedIds.indexOf(numberIndex) >= 0;
     }
 
     selectNumber = (numberIndex) => {
       this.setState((prevState) => ({
-        selectedNumbers: [...prevState.selectedNumbers, numberIndex],
+        selectedIds: [...prevState.selectedIds, numberIndex],
       }));
     }
 
+    gameStatus = () => {
+      const sum = this.state.selectedIds.reduce((acc,curr) => {
+        return acc + this.randomNumbers[curr];
+      },0);
+
+      if(sum < this.target) {
+        return 'PLAYING';
+      }
+
+      if(sum === this.target) {
+        return 'WON';
+      }
+
+      if(sum > this.target) {
+        return 'LOST';
+      }
+    }
+
     render() {
+      const gameStatus = this.gameStatus();
       return (
         <View style={styles.container}>
-          <Text style={styles.target}>{this.target}</Text>
+          <Text style={[styles.target, styles[`STATUS_${gameStatus}`]]}>{this.target}</Text>
           <View style={styles.randomNumberContainer}>
             {this.randomNumbers.map((randomNumber, index) => (
               <RandomNumber
                 key={index}
                 id={index}
                 number={randomNumber}
-                isDisabled={this.isNumberSelected(index)}
+                isDisabled={this.isNumberSelected(index) || gameStatus !== 'PLAYING'}
                 onPress={this.selectNumber}/>
             ))}
           </View>
-
+          <Text>{gameStatus}</Text>
         </View>
       );
     }
@@ -59,7 +78,6 @@ const styles = StyleSheet.create({
   },
   target: {
     fontSize: 50,
-    backgroundColor: '#bbb',
     marginHorizontal: 50,
     textAlign: 'center',
   },
@@ -76,6 +94,15 @@ const styles = StyleSheet.create({
     marginVertical: 25,
     textAlign: 'center',
     fontSize: 35
+  },
+  STATUS_PLAYING: {
+    backgroundColor: '#bbb',
+  },
+  STATUS_WON: {
+    backgroundColor: 'green',
+  },
+  STATUS_LOST: {
+    backgroundColor: 'red',
   }
 });
 
