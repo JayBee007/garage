a635fcb561ceaca955301e0c0e9a02d5adbfac95
Jay <jaybee.codes@gmail.com>
2017-11-23 09:14:37 +0300

    add styles DealItem

--- dataDrivenRN/src/components/DealItem.js
+++ dataDrivenRN/src/components/DealItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 import { priceDisplay } from '../utils';
 
@@ -10,31 +10,55 @@ class DealItem extends React.Component {
     deal: PropTypes.object.isRequired
   }
 
+  handlePress = () => {
+    console.log(this.props.deal.key);
+  }
+
   render() {
     const { deal } = this.props;
     return(
-      <View>
-        <Image
-          style={styles.image}
-          source={{uri: deal.media[0]}}
-        />
+      <TouchableOpacity style={styles.deal} onPress={this.handlePress}>
+        <Image style={styles.image} source={{uri: deal.media[0]}} />
 
-        <View>
-          <Text>{deal.title}</Text>
-          <Text>{priceDisplay(deal.price)}</Text>
-          <Text>{deal.cause.name}</Text>
+        <View style={styles.info}>
+          <Text style={styles.title}>{deal.title}</Text>
+
+          <View style={styles.footer}>
+            <Text style={styles.cause}>{deal.cause.name}</Text>
+            <Text style={styles.price}>{priceDisplay(deal.price)}</Text>
+          </View>
+
         </View>
-      </View>
-
+      </TouchableOpacity>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  deal: {
+    marginHorizontal: 12,
+    marginVertical: 12,
+  },
+  info: {
+    padding: 10,
+    backgroundColor: '#fff',
+    borderColor: 'black',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderTopWidth: 0,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
   image: {
     width: '100%',
-    height: 150
-
+    height: 150,
+    backgroundColor: '#ccc',
   }
 });
 
