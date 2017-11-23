8089b7355d14dc1fd72349359c48f6e97d58afd6
Jay <jaybee.codes@gmail.com>
2017-11-23 22:05:26 +0300

    add styles DealDetail

--- dataDrivenRN/src/components/DealDetail.js
+++ dataDrivenRN/src/components/DealDetail.js
@@ -27,20 +27,29 @@ class DealDetail extends React.Component {
   render() {
     const { deal } = this.state;
     return(
-      <TouchableOpacity style={styles.deal} onPress={this.handlePress}>
-        <Image style={styles.image} source={{uri: deal.media[0]}} />
-
-        <View style={styles.info}>
-          <Text style={styles.title}>{deal.title}</Text>
-
-          <View style={styles.footer}>
-            <Text style={styles.cause}>{deal.cause.name}</Text>
-            <Text style={styles.price}>{priceDisplay(deal.price)}</Text>
+      <View style={styles.deal}>
+        <Image source={{ uri: deal.media[0] }} style={styles.image} />
+        <View style={styles.detail}>
+          <View>
+            <Text style={styles.title}>{deal.title}</Text>
           </View>
-
+          <View style={styles.footer}>
+            <View style={styles.info}>
+              <Text style={styles.price}>{priceDisplay(deal.price)}</Text>
+              <Text style={styles.cause}>{deal.cause.name}</Text>
+            </View>
+            {deal.user && (
+              <View style={styles.user}>
+                <Image source={{ uri: deal.user.avatar }} style={styles.avatar} />
+                <Text>{deal.user.name}</Text>
+              </View>
+            )}
+          </View>
+          <View style={styles.description}>
+            <Text>{deal.description}</Text>
+          </View>
         </View>
-        <Text>Text....</Text>
-      </TouchableOpacity>
+      </View>
     );
   }
 }
@@ -48,29 +57,53 @@ class DealDetail extends React.Component {
 const styles = StyleSheet.create({
   deal: {
     marginHorizontal: 12,
-    marginVertical: 12,
-  },
-  info: {
-    padding: 10,
-    backgroundColor: '#fff',
-    borderColor: 'black',
-    borderWidth: StyleSheet.hairlineWidth,
-    borderTopWidth: 0,
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 4,
-  },
-  footer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    marginTop: 50,
+    borderColor: '#bbb',
+    borderWidth: 1,
   },
   image: {
     width: '100%',
     height: 150,
     backgroundColor: '#ccc',
-  }
+  },
+  detail: {
+  },
+  title: {
+    fontSize: 16,
+    padding: 10,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(237, 149, 45, 0.4)',
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 15,
+  },
+  info: {
+    alignItems: 'center',
+  },
+  user: {
+    alignItems: 'center',
+  },
+  cause: {
+    marginVertical: 10,
+  },
+  price: {
+    fontWeight: 'bold'
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+  },
+  description: {
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderStyle: 'dotted',
+    margin: 10,
+    padding: 10,
+  },
 });
 
 export default DealDetail;
